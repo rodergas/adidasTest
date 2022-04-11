da30b47e99164d2bc38bf0dd5b7aeb7cfd897b2d
rodergas <robertalmar.14@gmail.com>
2022-04-11 13:03:28 +0200

    Working docker compose with postgres integration, also test not failing while building docker image

--- subscription/src/test/java/com/adidas/test/subscription/SubscriptionServiceApplicationTests.java
+++ subscription/src/test/java/com/adidas/test/subscription/SubscriptionServiceApplicationTests.java
@@ -2,12 +2,26 @@ package com.adidas.test.subscription;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@DirtiesContext
 class SubscriptionServiceApplicationTests {
+	@Container
+	public static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:13.1-alpine").withDatabaseName("postgres");
 
-	@Test
-	void contextLoads() {
+	@DynamicPropertySource
+	public static void properties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url",postgresDB::getJdbcUrl);
+		registry.add("spring.datasource.username", postgresDB::getUsername);
+		registry.add("spring.datasource.password", postgresDB::getPassword);
+
 	}
 
 }
