c2e84da8f44cd65928d10102d0c867da34a5bdfb
rodergas <robertalmar.14@gmail.com>
2022-04-11 07:32:20 +0200

    Refactor packages path

--- public/src/main/java/com/adidas/test/publicservice/PublicApplication.java
+++ public/src/main/java/com/adidas/test/publicservice/PublicApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.adidas.test.publicservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
