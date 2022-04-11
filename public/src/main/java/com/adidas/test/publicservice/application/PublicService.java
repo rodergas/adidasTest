ecd3d013d27c419cfb96080cec72f4c67dc3a426
rodergas <robertalmar.14@gmail.com>
2022-04-11 13:41:08 +0200

    Connect public service to subscription service

--- public/src/main/java/com/adidas/test/publicservice/application/PublicService.java
+++ public/src/main/java/com/adidas/test/publicservice/application/PublicService.java
@@ -2,11 +2,14 @@ package com.adidas.test.publicservice.application;
 
 import com.adidas.test.publicservice.domain.Subscription;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Service
 public class PublicService {
@@ -22,6 +25,27 @@ public class PublicService {
 
         ResponseEntity<Subscription> response = restTemplate
                 .exchange(subscriptionServiceURL + "/subscriptions", HttpMethod.POST, new HttpEntity<>(subscription), Subscription.class);
+        return response.getBody();
+    }
+
+    public Iterable<Subscription> getAllSubscriptions(){
+
+        ResponseEntity<Iterable<Subscription>> response = restTemplate
+                .exchange(subscriptionServiceURL + "/subscriptions", HttpMethod.GET, null, new ParameterizedTypeReference<Iterable<Subscription>>() {});
+        return response.getBody();
+    }
+
+    public Optional<Subscription> getSubscriptionById(Long id){
+
+        ResponseEntity<Optional<Subscription>> response = restTemplate
+                .exchange(subscriptionServiceURL + String.format("/subscriptions/%s",id), HttpMethod.GET, null, new ParameterizedTypeReference<Optional<Subscription>>() {});
+        return response.getBody();
+    }
+
+    public String cancelSubscription(Long id){
+
+        ResponseEntity<String> response = restTemplate
+                .exchange(subscriptionServiceURL + String.format("/subscriptions/%s",id), HttpMethod.DELETE, null, String.class);
         return response.getBody();
     }
 }
--- public/src/main/java/com/adidas/test/publicservice/infrastructure/PublicController.java
+++ public/src/main/java/com/adidas/test/publicservice/infrastructure/PublicController.java
@@ -4,6 +4,8 @@ import com.adidas.test.publicservice.application.PublicService;
 import com.adidas.test.publicservice.domain.Subscription;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/subscriptions")
 public class PublicController {
@@ -16,21 +18,21 @@ public class PublicController {
 
     @PostMapping
     public Subscription createNewSubscription(@RequestBody Subscription subscription) {
-        return this.publicService.createSubscription(subscription);
+        return publicService.createSubscription(subscription);
     }
 
     @DeleteMapping("/{id}")
-    public String cancelSubscriptionById(@PathVariable String id) {
-        return "ID: ";
+    public String cancelSubscriptionById(@PathVariable Long id) {
+        return publicService.cancelSubscription(id);
     }
 
     @GetMapping("/{id}")
-    public String getSubscriptionById(@PathVariable String id) {
-        return "ID: " + id;
+    public Optional<Subscription> getSubscriptionById(@PathVariable Long id) {
+        return publicService.getSubscriptionById(id);
     }
 
     @GetMapping
-    public String getAllSubscriptions() {
-        return "All subscriptions ";
+    public Iterable<Subscription> getAllSubscriptions() {
+        return publicService.getAllSubscriptions();
     }
 }
